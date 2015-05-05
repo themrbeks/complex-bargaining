bc4830c28a32d87552a61768fb5cfaa75f7b617d
Stjepan <stjepan.begusic@hotmail.com>
2015-05-05 09:03:06 +0200

    New nodes only connect to non-first cluster nodes.

--- complex-bargaining-model/src/main/java/com/model/Main.java
+++ complex-bargaining-model/src/main/java/com/model/Main.java
@@ -3,9 +3,14 @@ package com.model;
 import com.jmatio.io.MatFileWriter;
 import com.jmatio.types.MLArray;
 import com.jmatio.types.MLDouble;
+import com.jmatio.types.MLStructure;
 import com.model.bargaining.Market;
 import com.model.bargaining.Util;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -40,8 +45,33 @@ System.out.print("\nExporting data to mat file...");
         c.add(test.exportDemandPrices("demandPrices"));
         c.add(test.exportSupplyPrices("supplyPrices"));
 
+
+        c.add(exportParametersToStruct());
+
+
         new MatFileWriter("output.mat", c);
 System.out.print("\nDone.\n");
         System.out.println(Util.kolikoPuta);
+    }
+
+    public static MLStructure exportParametersToStruct () throws IOException {
+        MLStructure mlStruct = new MLStructure("parameters", new int[] {1,1} );
+
+        FileReader fStream = new FileReader("parameters.txt");
+        BufferedReader in = new BufferedReader(fStream);
+
+        String line = in.readLine();
+        while (line != null){
+            if (!line.split(":")[0].equals("realPriceFileName")) {
+                double[] vals = new double[1];
+                vals[0] = Double.parseDouble(line.split(":")[1]);
+                MLDouble MLvals = new MLDouble(null,vals,1);
+                mlStruct.setField(line.split(":")[0], MLvals);
+            }
+            line = in.readLine();
+        }
+        fStream.close();
+        in.close();
+        return mlStruct;
     }
 }
